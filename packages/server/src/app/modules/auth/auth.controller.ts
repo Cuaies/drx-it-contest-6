5d6d53cfde1b7184ecd564d0aff976cee6a10083
Cuaies <61624423+Cuaies@users.noreply.github.com>
2025-03-12 17:27:26 +0200

    feat(routes): amend hardcoded routing

--- packages/server/src/app/modules/auth/auth.controller.ts
+++ packages/server/src/app/modules/auth/auth.controller.ts
@@ -8,21 +8,23 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { getLoginDto, getRegisterDto } from '@drx-it-contest-6/core';
+// skipcq: JS-0257
 import { Response } from 'express';
+import { Routes } from '../../../core/constants';
 
 export class RegisterDto extends getRegisterDto() {}
 export class LoginDto extends getLoginDto() {}
 
-@Controller('auth')
+@Controller(Routes.Users.Base)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
+  @Post(Routes.Users.Register)
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
 
-  @Post('login')
+  @Post(Routes.Users.Login)
   login(
     @Res({ passthrough: true }) response: Response,
     @Body() loginDto: LoginDto,
@@ -30,7 +32,7 @@ export class AuthController {
     return this.authService.login(response, loginDto);
   }
 
-  @Post('logout')
+  @Post(Routes.Users.Logout)
   @HttpCode(HttpStatus.OK)
   logout(@Res({ passthrough: true }) response: Response) {
     return this.authService.logout(response);
---
+++ packages/server/src/core/constants/routes.constants.ts
@@ -0,0 +1,8 @@
+export const Routes = {
+  Users: {
+    Base: '/auth', // TODO: fix
+    Register: '/register',
+    Login: '/login',
+    Logout: '/logout',
+  },
+} as const;
