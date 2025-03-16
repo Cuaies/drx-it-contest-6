fe1b4d52000e997659a01addae16fbf49397f5c2
Cuaies <61624423+Cuaies@users.noreply.github.com>
2025-03-17 00:15:41 +0200

    feat(stages): add pagination

--- packages/server/src/app/modules/stages/stages.service.ts
+++ packages/server/src/app/modules/stages/stages.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { Stage } from './models/stage.model';
 import { InjectModel } from '@nestjs/sequelize';
+import { getPaginationDto } from '@drx-it-contest-6/core';
+import { PaginationService } from '../pagination/pagination.service';
+
+export class PaginationDto extends getPaginationDto() {}
 
 @Injectable()
 export class StagesService {
   constructor(
     @InjectModel(Stage)
     private stageModel: typeof Stage,
+    private paginationService: PaginationService,
   ) {}
 
-  async getStages() {
-    return this.stageModel.findAll();
+  getStages(paginationDto: PaginationDto) {
+    return this.paginationService.paginate(paginationDto, this.stageModel, {});
   }
 }
