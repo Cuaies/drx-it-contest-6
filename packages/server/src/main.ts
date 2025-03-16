a7b66b16758d3bb97eae05eb1345aefa6d0cdde1
cuaies <61624423+Cuaies@users.noreply.github.com>
2025-03-16 11:58:28 +0200

    feat(filters): implement exception filter (#7)

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,16 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { GeneralExceptionsFilter } from './core/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapter = app.get(HttpAdapterHost);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+  app.useGlobalFilters(new GeneralExceptionsFilter(httpAdapter));
   app.use(cookieParser());
   await app.listen(3000);
 }
