68ec31e2e1c38f15a5b42589d52289727137b622
cuaies <61624423+Cuaies@users.noreply.github.com>
2025-03-20 18:42:32 +0200

    feat: create loader component

--- packages/client/app/components/index.ts
+++ packages/client/app/components/index.ts
@@ -1,1 +1,4 @@
 export * from "./authProvider/authProvider";
+export * from "./table";
+export * from "./rows";
+export * from "./loader";
---
+++ packages/client/app/components/loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="w-8 h-8 border-3 border-t-transparent border-teal-600 border-solid rounded-full animate-spin"></div>
+    </div>
+  );
+};
