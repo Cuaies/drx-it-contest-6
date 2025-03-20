6263cb221bfd87c4d0685da236f2949d484a6171
cuaies <61624423+Cuaies@users.noreply.github.com>
2025-03-20 16:16:46 +0200

    feat(client): create navbar

---
+++ packages/client/app/components/navbar.tsx
@@ -0,0 +1,31 @@
+import { LuCircleUserRound } from "react-icons/lu";
+import { useProvideAuth } from "../hooks";
+import ThemeToggleButton from "./themeToggle";
+
+export const Navbar = () => {
+  const { user } = useProvideAuth();
+
+  return (
+    <div className="flex items-center justify-between !px-4 !py-2 bg-white rounded-xl !m-2 shadow-xs bg-white text-black dark:text-white dark:bg-gray-900">
+      <div className=""></div>
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center !mx-4">
+          <ThemeToggleButton />
+        </div>
+        <div className="flex items-center justify-center gap-2 text-xl bold">
+          {user && (
+            <div className="flex flex-col">
+              <span className="font-semibold text-[16px]">
+                PLACEHOLDER NAME
+              </span>
+              <span className="text-[12px] text-gray-500 text-right">
+                PLACEHOLDER ROLE
+              </span>
+            </div>
+          )}
+          <LuCircleUserRound size={30} />
+        </div>
+      </div>
+    </div>
+  );
+};
--- packages/client/app/layouts/generic.tsx
+++ packages/client/app/layouts/generic.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router";
 import { Menu } from "../components/menu";
 import { GrCode } from "react-icons/gr";
+import { Navbar } from "../components/navbar";
 
 const GenericLayout = () => {
   return (
-    <div className="h-screen flex">
-      <div className="w-1/6 md:w[8%] lg:w-[16%] xl:w-[14%]">
+    <div className="h-screen flex bg-white text-black dark:text-white dark:bg-gray-900">
+      <div className="w-1/6 md:w[8%] lg:w-[16%] xl:w-[14%] shadow-xl">
         <a
           className="logo flex items-center justify-center gap-4 !m-4"
           href="/"
@@ -15,7 +16,8 @@ const GenericLayout = () => {
         </a>
         <Menu />
       </div>
-      <div className="w-5/6 md:w[92%] lg:w-[84%] xl:w-[86%] bg-stone-100">
+      <div className="w-5/6 md:w[92%] lg:w-[84%] xl:w-[86%] bg-gray-50 text-black dark:text-white dark:bg-gray-800">
+        <Navbar />
         <Outlet />
       </div>
     </div>
